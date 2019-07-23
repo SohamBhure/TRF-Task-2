1cb34ff4e12fbf33f45e38f150b0421464b0db14
RitvikPatil <53184106+RitvikPatil@users.noreply.github.com>
2019-07-23 17:53:21 +0530

    Update webScrapping_NEWS_2.py

--- webScrapping_NEWS_2.py
+++ webScrapping_NEWS_2.py
@@ -4,11 +4,10 @@ import requests
 
 print()
 
-x = input("Enter News to Search: ").split()
-
+x = input("Enter News to Search: ").split()   #Inputs news topics to be searched
 x = ("%20".join(x))
 
-site_Google = "https://news.google.com/search?q=" + str(x) + "&hl=en-IN&gl=IN&ceid=IN%3Aen"
+site_Google = "https://news.google.com/search?q=" + str(x) + "&hl=en-IN&gl=IN&ceid=IN%3Aen"   #Creates urls to be parsed
 site_IndiaToday = "https://www.indiatoday.in/topic/" + str(x)
 site_NDTV = "https://www.ndtv.com/topic/" + str(x)
 site_TOI = "https://timesofindia.indiatimes.com/topic/" + str(x) + "/"
@@ -21,14 +20,14 @@ print()
 print ("GOOGLE NEWS :")
 
 response = requests.get(site_Google)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-counter = 1
-posts = soup.find_all('h3', class_ = "ipQwMb ekueJc RD0gLb")
+soup = BeautifulSoup(response.text, 'html.parser')   #passes 
 
 
+posts = soup.find_all('h3', class_ = "ipQwMb ekueJc RD0gLb")    #finds all tags having given CSS class
 
-for i in range (1,6):
+
+
+for i in range (1,6):           #prints top5 news articles 
    print(str(i) + ". " + posts[i-1].get_text())
 
 
@@ -42,10 +41,10 @@ print ("INDIA TODAY NEWS :")
 response = requests.get(site_IndiaToday)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-counter = 1
-posts = soup.find_all('div', class_ = "views-field views-field-php-2")
 
-for i in range (1,6):
+posts = soup.find_all('div', class_ = "views-field views-field-php-2")    #finds all tags having given CSS class
+
+for i in range (1,6):           #prints top5 news articles 
    print(str(i) + ". " + posts[i-1].get_text())
 
 
@@ -59,10 +58,10 @@ print ("TIMES OF INDIA NEWS :")
 response = requests.get(site_TOI)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-counter = 1
-posts = soup.find_all('div', class_ = "content")
 
-for i in range (1,6):
+posts = soup.find_all('div', class_ = "content")     #finds all tags having given CSS class
+
+for i in range (1,6):           #prints top5 news articles 
    print(str(i) + ". " + posts[i-1].get_text()[1::])
 
 print()
@@ -75,12 +74,12 @@ print()
 print ("NDTV NEWS :")
 
 response = requests.get(site_NDTV)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')    #finds all tags having given CSS class
 
-counter = 1
+
 posts = soup.find_all('p', class_ = "intro")
 
-for i in range (1,6):
+for i in range (1,6):           #prints top5 news articles 
    print(str(i) + ". " + posts[i-1].get_text()[25::])
 
 
