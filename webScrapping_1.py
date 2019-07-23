3f45e81b50063c6eebd8b160d9f1df0a00f6e1f5
RitvikPatil <53184106+RitvikPatil@users.noreply.github.com>
2019-07-23 17:53:52 +0530

    Update webScrapping_1.py

--- webScrapping_1.py
+++ webScrapping_1.py
@@ -1,12 +1,13 @@
+#Importing necessary libraries
 import requests
 from bs4 import BeautifulSoup
 
-print()
+print()   ####
 
-search = input("Enter Word to Search: ").split()
+search = input("Enter Word to Search: ").split()  #Inputs the word to be searched
 
 site = "https://www.dictionary.com/browse/"
-for x in search:
+for x in search:                    #Creates the url to be parsed
     site += str(x)
 
 response = requests.get(site)
@@ -14,15 +15,15 @@ soup = BeautifulSoup(response.text,'html.parser')
 
 print()
 
-if (soup.find_all(class_="css-1o58fj8 e1hk9ate4")):
+if (soup.find_all(class_="css-1o58fj8 e1hk9ate4")):  #for valid word
 
     counter = 1
     posts = soup.find_all(class_="css-1o58fj8 e1hk9ate4")
-    for post in posts:
+    for post in posts:       #prints all definations from the website
 
         print(str(counter) + ". " + post.get_text())
         counter += 1
 
-else:
+else:      #for invalid word
     print("Enter Valid Word")
 
